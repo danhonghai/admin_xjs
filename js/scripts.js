33c5c253f58683204c08caa32a30b6a8e5df97ae
wuyongfang <954202321@qq.com>
2017-10-09 10:15:34 +0800

    代码优化

--- js/scripts.js
+++ js/scripts.js
@@ -412,3 +412,52 @@ function  confirmFun(paramsObj){
     });
 }
 
+//清空表单
+function clearFrom(id){
+    var objId = document.getElementById(id);
+    objId.reset();
+    if (objId == undefined) {
+        return;
+    }
+    for (var i = 0; i < objId.elements.length; i++) {
+        if (objId.elements[i].type == "text") {
+            objId.elements[i].value = "";
+        }
+        else if (objId.elements[i].type == "password") {
+            objId.elements[i].value = "";
+        }
+        else if (objId.elements[i].type == "email") {
+            objId.elements[i].value = "";
+        }
+        else if (objId.elements[i].type == "radio") {
+
+            $('#'+id).find('.icheck').find("input[type='radio']").eq(0).iCheck('check');
+        }
+        else if (objId.elements[i].type == "checkbox") {
+            $(objId.elements[i]).iCheck('uncheck');
+        }
+        else if (objId.elements[i].type == "select-one") {
+            objId.elements[i].options[0].selected = true;
+        }
+        else if (objId.elements[i].type == "select-multiple") {
+            for (var j = 0; j < objId.elements[i].options.length; j++) {
+                objId.elements[i].options[j].selected = false;
+            }
+        }
+        else if (objId.elements[i].type == "textarea") {
+            objId.elements[i].value = "";
+        }
+        //else if (objId.elements[i].type == "file") {
+        // //objId.elements[i].select();
+        // //document.selection.clear();
+        // // for IE, Opera, Safari, Chrome
+        // var file = objId.elements[i];
+        // if (file.outerHTML) {
+        // file.outerHTML = file.outerHTML;
+        // } else {
+        // file.value = ""; // FF(包括3.5)
+        // }
+        //}
+    }
+}
+
